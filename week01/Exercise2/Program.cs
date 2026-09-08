a75ff8453f6b5f8e5c564a885f316c1f8b8f68c1
jedkw06 <jedkw06@gmail.com>
2026-09-08 23:03:16 +0200

    exercise 2 complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What grade percentage did you score on the test? ");
+        string  userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("You recieved an A");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("You recieved a B");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You received a C");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("You recieved a D");
+        }
+        else
+        {
+            Console.WriteLine("You recieved an F");
+        }
+            if (gradePercentage >= 70)
+    {
+        Console.WriteLine("Congratulations! You passed the test.");
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you did not pass the test.");
+    }
+        
     }
 }
