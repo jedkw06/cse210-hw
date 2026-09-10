875c68657be41ddfdfe62fe66cec259225e2ad23
jedkw06 <jedkw06@gmail.com>
2026-09-10 17:19:18 +0200

    exercise 3,4,5 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 1000);
+        Console.WriteLine("Try and guess the magic number between 1 and 1000.");
+        Console.Write("What is your guess?" ); 
+        string userInput = Console.ReadLine();
+        int userGuess = int.Parse(userInput);
+        while (userGuess != magicNumber)
+        {
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess? ");
+            userInput = Console.ReadLine();
+            userGuess = int.Parse(userInput);
+        }
+        Console.WriteLine("Congratulations! You guessed the magic number.");
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter a number: ");
+            userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+        }
+        Console.WriteLine("The average is: " + numbers.Average());
+        Console.WriteLine("The sum is: " + numbers.Sum());
+        Console.WriteLine("The largest number is: " + numbers.Max());
     }
 }
